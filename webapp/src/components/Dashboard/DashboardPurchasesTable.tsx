bffe06eefa8b51d07167dfe3ae4ad2769e775c2b
FranciscoGSilverio <franciscogsilverio@gmail.com>
2023-06-30 18:01:47 -0300

    Added the table of pending purchases in the dashboard

---
+++ webapp/src/components/Dashboard/DashboardPurchasesTable.tsx
@@ -0,0 +1,99 @@
+import { Table } from "reactstrap";
+
+import { BsCheck, BsFillTrashFill } from "react-icons/bs";
+import { BiErrorCircle } from "react-icons/bi";
+
+import { formatDate } from "../../utils/formatDate";
+import { Purchase } from "../../types/purchase";
+
+import { useMutation, useQueryClient } from "react-query";
+import axios from "axios";
+import { openModal as openResultModal } from "../Common/SweetAlerts";
+
+type TableProps = {
+  data: Purchase[];
+};
+
+const DashboardPurchasesTable = ({ data }: TableProps) => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const queryClient = useQueryClient();
+
+  // const { mutate: deleteClient } = useMutation({
+  //   mutationFn: (clientId: string) =>
+  //     axios.delete(`${apiUrl}/clients/${clientId}`),
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries("clients");
+  //     openResultModal(
+  //       true,
+  //       "Usuário deletado com sucesso!",
+  //       "O usuário foi deletado com sucesso"
+  //     );
+  //   },
+  //   onError: () => {
+  //     openResultModal(
+  //       false,
+  //       "Erro ao deletar usuário!",
+  //       "Algo deu errado ao deletar o usuário, tente novamente mais tarde ou contate o suporte"
+  //     );
+  //   },
+  // });
+
+  return (
+    <Table className="text-center">
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Data da compra</th>
+          <th>Data do último pagamento</th>
+          <th>Valor pendente</th>
+          <th>Cliente</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data &&
+          data.map((purchase) => {
+            const { day, month, year } = formatDate(purchase.createdAt);
+            const {
+              day: latestPaymentDay,
+              month: latestPaymentMonth,
+              year: latestPaymentYear,
+            } = formatDate(purchase.latestPaymentDate);
+
+            const formatedBuyDate = `${day}/${month}/${year}`;
+            const formatedLatestPaymentDate = `${latestPaymentDay}/${latestPaymentMonth}/${latestPaymentYear}`;
+
+            return (
+              <tr
+                key={purchase.purchaseId}
+                style={{ cursor: "pointer" }}
+                // onClick={() => openModal(client.clientId)}
+              >
+                <td>{purchase.productName}</td>
+                <td>{purchase.price}</td>
+                <td>{purchase.quantity}</td>
+                <td>{formatedBuyDate}</td>
+                <td>{formatedLatestPaymentDate}</td>
+                <td>{purchase.debtValue}</td>
+                <td>--</td>
+                <td>
+                  <BsFillTrashFill
+                    size={22}
+                    className="text-danger mx-1"
+                    // onClick={(event: React.MouseEvent<HTMLElement>) => {
+                    //   event.stopPropagation();
+                    //   deleteClient(client.clientId);
+                    // }}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </Table>
+  );
+};
+
+export default DashboardPurchasesTable;
--- webapp/src/pages/PaginaDashboard.tsx
+++ webapp/src/pages/PaginaDashboard.tsx
@@ -2,8 +2,11 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 import DashboardTopCard from "../components/Dashboard/DashboardTopCard";
+import DashboardPurchasesTable from "../components/Dashboard/DashboardPurchasesTable";
+import DefaultCard from "../components/Common/DefaultCard";
 
 import styled from "styled-components";
+import { CardBody, CardTitle } from "reactstrap";
 
 const DashboardTopCardContainer = styled.div`
   flex-grow: 1;
@@ -21,36 +24,49 @@ const PaginaDashboard = () => {
 
   if (isLoading || !dashboardData) return <></>;
   return (
-    <div className="d-flex" style={{ height: "150px" }}>
-      <DashboardTopCardContainer>
-        <DashboardTopCard
-          icon={1}
-          title={"Pagamentos pendentes"}
-          value={dashboardData.totalClientsInDebt}
-        />
-      </DashboardTopCardContainer>
-      <DashboardTopCardContainer>
-        <DashboardTopCard
-          icon={2}
-          title={"Compras efetuadas este mês"}
-          value={dashboardData.purchasesCreatedThisMonth}
-        />
-      </DashboardTopCardContainer>
-      <DashboardTopCardContainer>
-        <DashboardTopCard
-          icon={3}
-          title={"Compras pagas este mês"}
-          value={dashboardData.purchasesPaidThisMonth}
-        />
-      </DashboardTopCardContainer>
-      <DashboardTopCardContainer>
-        <DashboardTopCard
-          icon={4}
-          title={"Pagamentos atrasados"}
-          value={dashboardData.totalPastDues}
-        />
-      </DashboardTopCardContainer>
-    </div>
+    <>
+      <div className="d-flex" style={{ height: "150px" }}>
+        <DashboardTopCardContainer>
+          <DashboardTopCard
+            icon={1}
+            title={"Pagamentos pendentes"}
+            value={dashboardData.totalClientsInDebt}
+          />
+        </DashboardTopCardContainer>
+        <DashboardTopCardContainer>
+          <DashboardTopCard
+            icon={2}
+            title={"Compras efetuadas este mês"}
+            value={dashboardData.purchasesCreatedThisMonth}
+          />
+        </DashboardTopCardContainer>
+        <DashboardTopCardContainer>
+          <DashboardTopCard
+            icon={3}
+            title={"Compras pagas este mês"}
+            value={dashboardData.purchasesPaidThisMonth}
+          />
+        </DashboardTopCardContainer>
+        <DashboardTopCardContainer>
+          <DashboardTopCard
+            icon={4}
+            title={"Pagamentos atrasados"}
+            value={dashboardData.totalPastDues}
+          />
+        </DashboardTopCardContainer>
+      </div>
+
+      <div className="my-3">
+        <DefaultCard>
+          <CardBody>
+            <CardTitle tag="h5" className="pb-3">
+              Compras pendentes
+            </CardTitle>
+          </CardBody>
+          <DashboardPurchasesTable data={dashboardData.pendingPurchases} />
+        </DefaultCard>
+      </div>
+    </>
   );
 };
 
