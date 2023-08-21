28ac0e38e61e31bbcf9118aab10bf1948d69d26d
FranciscoGSilverio <franciscogsilverio@gmail.com>
2023-08-21 20:42:50 -0300

    update env variables

--- webapp/src/utils/auth/emailAndPasswordLogin.ts
+++ webapp/src/utils/auth/emailAndPasswordLogin.ts
@@ -2,22 +2,6 @@ import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "../../services/firebaseConfig";
 
 export const signInUser = (email: string, password: string) =>
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
+  signInWithEmailAndPassword(auth, email, password);
 
-export const signOutUser = () =>
-  signOut(auth)
-    .then(() => {
-      // Sign-out successful.
-    })
-    .catch((error) => {
-      // An error happened.
-    });
+export const signOutUser = () => signOut(auth);
