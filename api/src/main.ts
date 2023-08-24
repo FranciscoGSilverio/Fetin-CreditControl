b4acb72add6ff926c43e2036242b65840424f441
FranciscoGSilverio <franciscogsilverio@gmail.com>
2023-08-24 18:00:35 -0300

    Added https configuration for development environment

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const certificatePath = path.join(__dirname, '../openssl/certificate'); // Adjust the path here
+
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(certificatePath, 'server.key')),
+    cert: fs.readFileSync(path.join(certificatePath, 'server.crt')),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('FETIN - Credit Control API')
